f667b2d81a50493a80cc52eda2a4b95de25080ee
Daniel Jackson <daniel@snilldesign.no>
2022-08-16 14:03:44 +0200

    Simple front page, listing meetings, people and organizations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
     # Front page
+    @organizations = Organization.all
+    @meetings = Meeting.all.last(10)
+    @people = Person.includes(:organization).all
   end
 end
